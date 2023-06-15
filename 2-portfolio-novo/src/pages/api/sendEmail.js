1f16b1d427cc50c6d3e1c759648b37f95d15fb0e
anderecc <andersondbl06@gmail.com>
2023-06-15 12:01:30 -0300

    add sendgrid

--- 2-portfolio-novo/src/pages/api/sendEmail.js
+++ 2-portfolio-novo/src/pages/api/sendEmail.js
@@ -1,33 +1,34 @@
 /* eslint-disable no-undef */
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
+import sendgrid from '@sendgrid/mail';
 
-export default function sendEmail(req, res) {
+sendgrid.setApiKey(process.env.NEXT_PUBLIC_KEY_GRID);
+
+export default async function sendEmail(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Método não suportado.' });
     }
 
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.umbler.com',
-        auth: {
-            port: 587,
-            user: process.env.NEXT_PUBLIC_USER_MAIL,
-            pass: process.env.NEXT_PUBLIC_PASS_MAIL,
-        },
-    });
-
-    transporter
-        .sendMail({
-            from: `"${req.body.name}" <${process.env.NEXT_PUBLIC_USER_MAIL}>`, // sender address
-            to: process.env.NEXT_PUBLIC_USER_MAIL + ', andersondbl06@gmail.com', // list of receivers
-            replyTo: req.body.email,
-            subject: `Contato feito por '${req.body.name}' via Portfólio`, // Subject line
-            text: req.body.text, // plain text body
-            html: `<b>Nome de quem enviou: ${req.body.name}<br/> Email de quem enviou: ${req.body.email}</br><br/> Texto de quem enviou: ${req.body.text}`, // html body
-        })
-        .then((response) => {
-            res.status(200).send(response.data);
-        })
-        .catch((err) => {
-            res.status(400).send(err);
+    try {
+        await sendgrid
+            .send({
+                from: 'Anderson <anderson@anderecc.dev.br>',
+                to: `andersondbl06@gmail.com`,
+                cc: 'anderson@anderecc.dev.br',
+                subject: `Contato feito por '${req.body.name}' via Portfólio`,
+                replyTo: req.body.email,
+                text: req.body.text,
+                html: `<b>Nome de quem enviou: ${req.body.name}<br/> Email de quem enviou: ${req.body.email}</br><br/> Texto de quem enviou: ${req.body.text}`,
+            })
+            .then(() => {
+                res.status(202).send({ data: 'success' });
+            })
+            .catch((err) => {
+                res.status(400).send(err);
+            });
+    } catch (error) {
+        res.status(400).send({
+            error: 'Ocorreu algum erro ao tentar enviar sua mensagem.',
         });
+    }
 }
