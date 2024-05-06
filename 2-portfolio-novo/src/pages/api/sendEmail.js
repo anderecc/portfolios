8f7bb976fae12a6e406a16e120534a84a408db94
anderecc <andersondbl06@gmail.com>
2024-05-06 17:12:27 -0300

    refatorado endpoint para enviar e-mail

--- 2-portfolio-novo/src/pages/api/sendEmail.js
+++ 2-portfolio-novo/src/pages/api/sendEmail.js
@@ -10,24 +10,19 @@ export default async function sendEmail(req, res) {
     }
 
     try {
-        await sendgrid
-            .send({
-                from: 'Anderson <anderson@anderecc.dev.br>',
-                to: `andersondbl06@gmail.com`,
-                cc: 'anderson@anderecc.dev.br',
-                subject: `Contato feito por '${req.body.name}' via Portfólio`,
-                replyTo: req.body.email,
-                text: req.body.text,
-                html: `<b>Nome de quem enviou: ${req.body.name}<br/> Email de quem enviou: ${req.body.email}</br><br/> Texto de quem enviou: ${req.body.text}`,
-            })
-            .then(() => {
-                res.status(202).send({ data: 'success' });
-            })
-            .catch((err) => {
-                res.status(400).send(err);
-            });
+        await sendgrid.send({
+            from: 'Anderson <anderson@anderecc.dev.br>',
+            to: `andersondbl06@gmail.com`,
+            cc: 'anderson@anderecc.dev.br',
+            subject: `Contato feito por '${req.body.name}' via Portfólio`,
+            replyTo: req.body.email,
+            text: req.body.text,
+            html: `<b>Nome de quem enviou: ${req.body.name}<br/> Email de quem enviou: ${req.body.email}</br><br/> Texto de quem enviou: ${req.body.text}`,
+        });
+
+        return res.status(202).send({ data: 'success' });
     } catch (error) {
-        res.status(400).send({
+        return res.status(400).send({
             error: 'Ocorreu algum erro ao tentar enviar sua mensagem.',
         });
     }
